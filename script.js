5620b8020f67c9f869de2f195998e4c49d2cd3fb
Dev-Putra12 <putraagus1204@gmail.com>
2024-05-21 18:03:33 +0800

    update dom for navbar

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// split navbar content
+document.addEventListener("DOMContentLoaded", function() {
+  fetch('./Component/navbar.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('navbar').innerHTML = data;
+    });
+});
+
+// fetch json file
 document.addEventListener("DOMContentLoaded", async () => {
   const table = document.querySelector(".nyc-sales");
   // Memuat file JSON
