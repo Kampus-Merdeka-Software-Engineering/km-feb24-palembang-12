315b1df9c1bb68ff57cca5166ba2b2e8bf824b89
zxcvbnm0091 <nisrina.adila@student.president.ac.id>
2024-05-22 20:21:06 +0700

    chore: delete unused file

--- script.js
+++
@@ -1,8 +0,0 @@
-// split navbar content
-document.addEventListener("DOMContentLoaded", function() {
-  fetch('./Component/navbar.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('navbar').innerHTML = data;
-    });
-});
